d1b32c9642dcd4842950c89cc955ffcd1a5285b1
yuininja36-dotcom <yuininja36@gmail.com>
2026-06-18 10:41:00 +1000

    Configure Next.js for static export for Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['images.unsplash.com', 'plus.unsplash.com'],
   },
 }
